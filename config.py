fddb8649cf6908804a3a84c58b06a72cc23a2737
raghavhsh <gshs4061@gmail.com>
2025-01-28 00:46:44 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+BOT_TOKEN = "7862189230:AAHkqUBUtOIvENW1S0Ya5DZzQnQbTiEG1hU"
+ADMIN_IDS = ["7452239135"]
+GROUP_ID = -1002421977380  # Replace with your group ID
+GROUP_LINK = "https://t.me/+8Uy26VLnS3A2ZTg1"  # Replace with your group invite link
+# Default number of threads for attacks
+DEFAULT_THREADS = 800  # Set the default number of threads to use for attacks
